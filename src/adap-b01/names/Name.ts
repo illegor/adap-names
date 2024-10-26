13c4ba1d77e774215f024b3978d38161d099fa61
Illegor <igormirkov2003@gmail.com>
2024-10-26 23:35:22 +0200

    probably final

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -8,56 +8,77 @@ export class Name {
     private components: string[] = [];
     private delimiter: string = this.DEFAULT_DELIMITER;
 
+    /** @methodtype constructor-methos (convenience-method) */
     constructor(other: string[], delimiter?: string) {
-        this.components = other;
+        this.components = other; //falls leer, egal, Fehlerbehandlung in jeweiligen Methoden
         if(delimiter != null){
             this.delimiter = delimiter;
         }
-        //throw new Error("needs implementation");
-    }
-
-    /** Returns human-readable representation of Name instance */
-    public asNameString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation");
-    }
-
-    public getComponent(i: number): string {
-        if(i > -1 && i < this.components.length){
-        return this.components[i];
-
+        else{
+            this.delimiter = this.DEFAULT_DELIMITER; //Default, falls kein Delimiter mitgegeben, da optional
         }
-    
-        throw new Error("invalid index");
         
     }
 
-    public setComponent(i: number, c: string): void {
-        if(i > -1 && i < this.components.length){
-            this.components[i] = c;
-        }
-        //throw new Error("invalid index");
+    /** Returns human-readable representation of Name instance */
+    /** @methodtype conversion-method */
+    public asNameString(delimiter: string = this.delimiter): string {
+        
+        let res = "" //leerer String wird nach und nach gefüllt
+        if(this.components === null || this.getNoComponents() == 0) return res; //falls nichts in components --> nichts zum lesen da
+        for(let i = 0; i < this.getNoComponents()-1; i++){ //nur bis ...-1, um am Ende keinen überflüssigen delimiter zu haben
+            res += this.components[i];
+            res += delimiter;
+            
+            }
+        res += this.components[this.getNoComponents()-1]; //letztes Part hinzufügen
+            
+        return res;
+        
+        
     }
+    /** @methodtype get-method */
+    public getComponent(i: number): string {
+        
+        if(i > -1 && i < this.getNoComponents()){ //nur gültige Indizes erlauben
+        return this.components[i]; //component zurückgeben
 
+        }
+    
+        throw new Error("invalid index"); //falls Index ungültig --> Es kann nichts ausgegeben werden
+        
+    }
+    /** @methodtype set-method */
+    public setComponent(i: number, c: string): void {
+        if(i > -1 && i < this.getNoComponents()){ //nur gültige Indizes können auch geändert werden
+            this.components[i] = c; //Component ändern
+        }
+        throw new Error("invalid index"); //falls Index ungültig --> Es kann nichts geändert werden
+    }
+    /** @methodtype get-method */
     public getNoComponents(): number {
         return this.components.length;
-        throw new Error("needs implementation");
     }
 
     public insert(i: number, c: string): void {
-        this.components[i] = c;
-        //throw new Error("needs implementation");
+        let first = this.components.slice(0, i) //c soll an Stelle i hinzugefügt werden --> Components bis i erstmal kopieren
+        first.push(c); //c an Stelle i hinzufügen
+        this.components = first.concat(this.components.slice(i, this.getNoComponents())); //rest der Components hinzufügen
+        
+        
     }
 
     public append(c: string): void {
         this.components.push(c);
-        throw new Error("needs implementation");
+        
     }
 
     public remove(i: number): void {
-        if(i > -1 && i < this.components.length){
-            this.components[i] = "";
+        if(i > -1 && i < this.getNoComponents()){ 
+        let first = this.components.slice(0, i) //Component an Stelle i soll entfernt werden --> Components bis i erstmal kopieren
+        this.components = first.concat(this.components.slice(i+1, this.getNoComponents())); //i überspringen und Rest kopieren
         }
-        throw new Error("invalid index");
+        throw new Error("invalid index"); //falls Index ungültig --> Es kann nichts geändert werden
     }
 
 }
