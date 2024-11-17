d5c417b8d9799086965170527b5c2bc848353337
Illegor <igormirkov2003@gmail.com>
2024-11-17 12:20:32 +0100

    not tested yet

--- src/adap-b03/names/StringArrayName.ts
+++ src/adap-b03/names/StringArrayName.ts
@@ -7,27 +7,47 @@ export class StringArrayName extends AbstractName {
 
     constructor(other: string[], delimiter?: string) {
         super();
-        throw new Error("needs implementation");
+        if(other.length > 0 && other[0]!= ""){
+            this.components = other; 
+            }
+            if(delimiter != null){
+                this.delimiter = delimiter;
+            }
+           
     }
 
     getNoComponents(): number {
-        throw new Error("needs implementation");
+        return this.components.length
     }
 
     getComponent(i: number): string {
-        throw new Error("needs implementation");
+        if(i > -1 && i < this.getNoComponents()){ //nur gültige Indizes erlauben
+            return this.components[i]; //component zurückgeben
+    
+            }
+        
+            throw new Error("invalid index"); //falls Index ungültig --> Es kann nichts ausgegeben werden
     }
     setComponent(i: number, c: string) {
-        throw new Error("needs implementation");
+        if(i > -1 && i < this.getNoComponents()){ //nur gültige Indizes können auch geändert werden
+            this.components[i] = c; //Component ändern
+        }
     }
 
     insert(i: number, c: string) {
-        throw new Error("needs implementation");
+        if(i < 0 || i > this.getNoComponents()) return
+        let first = this.components.slice(0, i) //c soll an Stelle i hinzugefügt werden --> Components bis i erstmal kopieren
+        first.push(c); //c an Stelle i hinzufügen
+        this.components = first.concat(this.components.slice(i, this.getNoComponents())); //rest der Components hinzufügen
     }
     append(c: string) {
-        throw new Error("needs implementation");
+        this.components.push(c);
     }
     remove(i: number) {
-        throw new Error("needs implementation");
+        if(i > -1 && i < this.getNoComponents()){ 
+            let first = this.components.slice(0, i) //Component an Stelle i soll entfernt werden --> Components bis i erstmal kopieren
+            this.components = first.concat(this.components.slice(i+1, this.getNoComponents())); //i überspringen und Rest kopieren
+            }
+            //throw new Error("invalid index"); //falls Index ungültig --> Es kann nichts geändert werden
     }
 }
