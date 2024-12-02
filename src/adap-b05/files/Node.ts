093919ea9362c9c2dd3f3932b2a2d54fab382502
Illegor <igormirkov2003@gmail.com>
2024-12-02 11:28:05 +0100

    working

--- src/adap-b05/files/Node.ts
+++ src/adap-b05/files/Node.ts
@@ -77,75 +77,8 @@ export class Node {
         return sol;
     }
 
-        // Initialize a set to collect nodes that match the given basename.
-        /**let matchedNodes = new Set<Node>();
-
-        // Check if the current node matches the basename and add it to the set if it does.
-        if (this.getBaseName() == bn) {
-            matchedNodes.add(this);
-        }
-
-        // If the current node is a Directory, recursively search its children.
-        let parent = this.getParentNode();
 
 
 
-            // Call findNodes on each child
-            let children = Array.from(parent.getChildren());
-            for (const child of children) {
-                const childMatches = child.findNodes(bn);
-                for (const matchedNode of childMatches) {
-                    matchedNodes.add(matchedNode);
-                }
-            }
-
-        // Return the set of matched nodes.
-        return matchedNodes;
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let sol: Set<Node> = new Set();
-        let full = this.getFullName();
-        if(this.getBaseName() == bn){
-            sol.add(this);
-        }
-        if(this.getParentNode() === null){
-            return sol;
-        }
-
-        let sol2 = this.getParentNode().findNodes(bn);
-        sol2.forEach(node => sol.add(node));
-
-        this.assertClassInvariants();
-        return sol;
-        //throw new Error("needs implementation or deletion");
- */
-
-
-    protected assertClassInvariants(): void {
-        const bn: string = this.doGetBaseName();
-        this.assertIsValidBaseName(bn, ExceptionType.CLASS_INVARIANT);
-    }
-
-    protected assertIsValidBaseName(bn: string, et: ExceptionType): void {
-        const condition: boolean = (bn != "");
-        AssertionDispatcher.dispatch(et, condition, "invalid base name");
-    }
 
 }
